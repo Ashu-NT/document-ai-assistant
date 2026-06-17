a146a212a3a0876bb7894f71c87f1f320f8f53df
Ashu <ashufrancis673@gmail.com>
2026-06-18 00:31:13 +0200

    added alembic integration

--- src/infrastructure/db/__init__.py
+++ src/infrastructure/db/__init__.py
@@ -1,8 +1,1 @@
-from src.infrastructure.db.session import SessionLocal, engine
-from src.infrastructure.db.unit_of_work import SqlAlchemyUnitOfWork
-
-__all__ = [
-    "SessionLocal",
-    "SqlAlchemyUnitOfWork",
-    "engine",
-]
+__all__ : list[str]= []
