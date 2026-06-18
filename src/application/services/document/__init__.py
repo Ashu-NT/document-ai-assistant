58d60cffd02a5c020c0bedfe3ac42498387f798b
Ashu <ashufrancis673@gmail.com>
2026-06-18 22:04:54 +0200

    added document_application_service

--- src/application/services/document/__init__.py
+++ src/application/services/document/__init__.py
@@ -8,10 +8,14 @@ from src.application.services.document.duplicate_detection_service import (
 from src.application.services.document.document_registration_service import (
     DocumentRegistrationService,
 )
+from src.application.services.document.document_application_service import (
+    DocumentApplicationService,
+)
 
 __all__ = [
     "DocumentLookupService",
     "DuplicateDetectionResult",
     "DuplicateDetectionService",
     "DocumentRegistrationService",
+    "DocumentApplicationService",
 ]
